39f02e07ed18b25c8c0eee1a579ccb4c92105da0
franklingu <franklingujunchao@gmail.com>
2018-03-16 10:06:45 +0800

    Fix tests for py3.5 because of mock library change

--- tests/test_google_finance.py
+++ tests/test_google_finance.py
@@ -8,7 +8,10 @@ from .helpers import MockedResponse
 def test_gf_success_matching(get_mock):
     gf_matcher = GoogleFinanceNameMatcher()
     symbols = gf_matcher.match_by('Apple Inc.')
-    get_mock.assert_called_once()
+    try:
+        get_mock.assert_called_once()
+    except AttributeError:
+        assert get_mock.call_count == 1
     assert 'Apple Inc.' in symbols
     assert len(symbols['Apple Inc.']) == 1
     assert symbols['Apple Inc.'][0][0] == 'Apple Inc.'
@@ -19,18 +22,24 @@ def test_gf_success_matching(get_mock):
 def test_gf_matching_with_exception(get_mock):
     gf_matcher = GoogleFinanceNameMatcher()
     symbols = gf_matcher.match_by('Apple Inc.', sleep=0)
-    get_mock.assert_called()
+    assert get_mock.call_count == 3
     assert symbols['Apple Inc.'][0][0] == 'Apple Inc.'
     assert symbols['Apple Inc.'][0][1].ticker == 'AAPL'
     assert symbols['Apple Inc.'][0][1].country == 'US'
     symbols = gf_matcher.match_by('Apple Inc.', retry=2, sleep=0)
-    get_mock.assert_called()
+    try:
+        get_mock.assert_called()
+    except AttributeError:
+        assert get_mock.called
     assert not symbols['Apple Inc.']
 
 @patch('requests.get', return_value=MockedResponse('gf_zhihu.html'))
 def test_gf_multiple_matching(get_mock):
     gf_matcher = GoogleFinanceNameMatcher()
     symbols = gf_matcher.match_by('zhihu')
-    get_mock.assert_called_once()
+    try:
+        get_mock.assert_called_once()
+    except AttributeError:
+        assert get_mock.call_count == 1
     assert 'zhihu' in symbols
     assert len(symbols['zhihu']) == 7
--- tests/test_yahoo_finance.py
+++ tests/test_yahoo_finance.py
@@ -8,7 +8,10 @@ from .helpers import MockedResponse
 def test_yh_success_matching(get_mock):
     yh_matcher = YahooFinanceNameMatcher()
     symbols = yh_matcher.match_by('Apple')
-    get_mock.assert_called_once()
+    try:
+        get_mock.assert_called_once()
+    except AttributeError:
+        assert get_mock.call_count == 1
     assert 'Apple' in symbols
     assert symbols['Apple'][0][1].ticker == 'AAPL'
     assert symbols['Apple'][0][1].country == 'US'
@@ -17,19 +20,25 @@ def test_yh_success_matching(get_mock):
 def test_yh_matching_with_exception(get_mock):
     yh_matcher = YahooFinanceNameMatcher()
     symbols = yh_matcher.match_by('Apple', sleep=0)
-    get_mock.assert_called()
+    assert get_mock.call_count == 3
     assert 'Apple' in symbols
     assert symbols['Apple'][0][1].ticker == 'AAPL'
     assert symbols['Apple'][0][1].country == 'US'
     symbols = yh_matcher.match_by('Apple', retry=2, sleep=0)
-    get_mock.assert_called()
+    try:
+        get_mock.assert_called()
+    except AttributeError:
+        assert get_mock.called
     assert not symbols['Apple']
 
 @patch('requests.get', return_value=MockedResponse('yh_zhihu.json'))
 def test_gf_multiple_matching(get_mock):
     yh_matcher = YahooFinanceNameMatcher()
     symbols = yh_matcher.match_by('zhihu')
-    get_mock.assert_called_once()
+    try:
+        get_mock.assert_called_once()
+    except AttributeError:
+        assert get_mock.call_count == 1
     assert 'zhihu' in symbols
     assert symbols['zhihu'][0][1].ticker == '603156'
     assert symbols['zhihu'][0][1].country == 'CN'
