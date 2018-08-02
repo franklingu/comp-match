ae5bc5e9e10df63b5b5f2bea116e25957593560f
franklingu <franklingujunchao@gmail.com>
2018-08-02 09:09:57 +0800

    Fix broken test for py3.5

--- tests/test_init.py
+++ tests/test_init.py
@@ -10,6 +10,6 @@ def test_match():
     """
     match_res = match(['Apple'])
     assert 'Apple' in match_res
-    assert match_res['Apple'][0][0] == 'Apple Inc.'
+    assert match_res['Apple'][0][0].title() == 'Apple Inc.'
     assert match_res['Apple'][0][1].ticker == 'AAPL'
     assert match_res['Apple'][0][1].country_code == 'US'
